e2517ba0a6abf4648ecc07ec97f686338e25eabf
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-11-21 11:51:13 -0700

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    
+    function handleChange(e){
+        console.log(e.target='Entering password...')
+    }
+
     return (
-        <div></div>
+        <div><input type="password" onChange={handleChange} /></div>
     )
 }
 
